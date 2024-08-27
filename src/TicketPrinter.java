1bd07790b206fdb35a4f3ddf6a9192a78d335ea5
MatveevVV <Vladthemat@gmail.com>
2024-08-27 13:12:05 +0400

    Initial commit

---
+++ src/TicketPrinter.java
@@ -0,0 +1,13 @@
+public class TicketPrinter {
+    public static void main(String[] args) {
+        // Печать билетов от 200000 до 210000
+        for (int i = 200000; i <= 210000; i++) {
+            System.out.println(i);
+        }
+
+        // Печать билетов от 220000 до 235000
+        for (int i = 220000; i <= 235000; i++) {
+            System.out.println(i);
+        }
+    }
+}
