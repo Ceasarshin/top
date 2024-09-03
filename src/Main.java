48bb317acc4b704904f399e520eb51ea022cf523
MatveevVV <Vladthemat@gmail.com>
2024-09-03 09:52:53 +0400

    Home work lesson 7-8

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        // Объявление и инициализация переменных всех 8 типов примитивов
-        byte myByte = 100;
-        short myShort = 10000;
-        int myInt = 100000;
-        long myLong = 10000000000L;
-        float myFloat = 10.5f;
-        double myDouble = 10.55555;
-        char myChar = 'A';
-        boolean myBoolean = true;
+        // Создание сканнера для ввода данных из консоли.
+        Scanner scanner = new Scanner(System.in);
 
-        // Арифметические операции с целами числами типа int
-        int a = 10;
-        int b = 5;
+        //Запрос значений у пользователя.
+        System.out.print("Введите целое значение A : ");
+        int a = scanner.nextInt();
 
-        int sum = a + b;           // Сложение
-        int difference = a - b;    // Вычитание
-        int product = a * b;       // Умножение
-        int quotient = a / b;      // Деление
-        int remainder = a % b;     // Остаток от деления
-        int inkrement = ++a;       // Увеличения значения переменной "a" на 1. Инкремент.
-        int dekrement = --a;       // Уменьшение значения переменной "a" на 1. Декремент.
+        System.out.print("Введите целое значение B : ");
+        int b = scanner.nextInt();
+
+        // Арифметические операции с целыми числами типа int.
+        int sum = a + b;           // Сложение.
+        int difference = a - b;    // Вычитание.
+        int product = a * b;       // Умножение.
+        int quotient = a / b;      // Деление.
+        int remainder = a % b;     // Остаток от деления.
+        int increment = ++a;       // Увеличение значения переменной "a" на 1. Инкремент.
+        int decrement = --a;       // Уменьшение значения переменной "a" на 1. Декремент.
 
         // Вывод результатов в консоль
         System.out.println("Сложение: " + sum);
@@ -28,7 +28,30 @@ public class Main {
         System.out.println("Умножение: " + product);
         System.out.println("Деление: " + quotient);
         System.out.println("Остаток от деления: " + remainder);
-        System.out.println("Инкремент: " + inkrement);
-        System.out.println("Декремент:" + dekrement);
+        System.out.println("Инкремент: " + increment);
+        System.out.println("Декремент: " + decrement);
+
+        // Генерация случайных чисел при помощи класса Random и приведение их к типу char.
+        Random random = new Random();
+        char letter1 = (char) (random.nextInt(26) + 'A'); // Генерация случайной 1 буквы.
+        char letter2 = (char) (random.nextInt(26) + 'A'); // Генерация случайной 2 буквы.
+        char letter3 = (char) (random.nextInt(26) + 'A'); // Генерация случайной 3 буквы.
+        char letter4 = (char) (random.nextInt(26) + 'A'); // Генерация случайной 4 буквы.
+
+        // Вывод случайных символов в одно слово.
+        System.out.print("Случайное слово: ");
+        System.out.print(letter1);
+        System.out.print(letter2);
+        System.out.print(letter3);
+        System.out.print(letter4);
+        System.out.println(); // Пустая строка в качестве разделителя.
+
+        // Переполнение типа int.
+        long myLong = 10000000000L;
+        int myInt = (int) myLong;
+
+        // Вывод значения long и полученного int.
+        System.out.println("Значение long: " + myLong);
+        System.out.println("Переполненное значение int: " + myInt);
     }
 }
